e7412745a3248ea7c3b243b7275164765e19f337
dsetareh <dsetareh@vt.edu>
2021-06-24 08:52:03 +0000

    ip helper functions

--- startFuzzEnv.py
+++ startFuzzEnv.py
@@ -1,52 +1,100 @@
 # startFuzzEnv.py
 import yaml, sys
 
-if len(sys.argv) != 4:
-    print(
-        "./startFuzzEnv.py [test to run] [subnet] [output docker-compose filename]"
-    )
-    quit(1)
 
-with open(
-        "/home/dsetareh/docker/srsRAN-docker-emulated/docker-compose-template.yml",
-        'r') as ymlfile:
-    try:
-        docker_config = yaml.safe_load(ymlfile)
-    except yaml.YAMLError as exc:
-        print(exc)
+# "/home/dsetareh/docker/srsRAN-docker-emulated/docker-compose-template.yml"
+def generate_compose(templateFilename, outputDir, testNumber):
+    with open(templateFilename, 'r') as ymlfile:
+        try:
+            docker_config = yaml.safe_load(ymlfile)
+        except yaml.YAMLError as exc:
+            print(exc)
 
-test_number = str(sys.argv[1])  # ! current test iteration
-subnet = str(sys.argv[2]) + '0/24'  # ! '10.80.95.0/24' subnet
-epc_ip = str(sys.argv[2]) + '10'  # ! '10.80.95.10'
-enb_ip = str(sys.argv[2]) + '11'  # ! '10.80.95.11'
+    test_number = str(sys.argv[1])  # ! current test iteration
+    subnet = str(sys.argv[2]) + '0/24'  # ! '10.80.95.0/24' subnet
+    epc_ip = str(sys.argv[2]) + '10'  # ! '10.80.95.10'
+    enb_ip = str(sys.argv[2]) + '11'  # ! '10.80.95.11'
 
-docker_config['services']['srsue'][
-    'command'] = 'stdbuf -oL srsue /etc/srsran/ue.conf.fauxrf -f' + test_number
+    docker_config['services']['srsue'][
+        'command'] = 'stdbuf -oL srsue /etc/srsran/ue.conf.fauxrf -f' + test_number
 
-docker_config['services']['srsepc'][
-    'command'] = 'stdbuf -oL srsepc /etc/srsran/epc.conf --mme.mme_bind_addr=' + epc_ip + ' --spgw.gtpu_bind_addr=' + epc_ip
+    docker_config['services']['srsepc'][
+        'command'] = 'stdbuf -oL srsepc /etc/srsran/epc.conf --mme.mme_bind_addr=' + epc_ip + ' --spgw.gtpu_bind_addr=' + epc_ip
 
-docker_config['services']['srsepc']['networks']['corenet'][
-    'ipv4_address'] = epc_ip
+    docker_config['services']['srsepc']['networks']['corenet'][
+        'ipv4_address'] = epc_ip
 
-docker_config['services']['srsenb'][
-    'command'] = 'srsenb /etc/srsran/enb.conf.fauxrf --enb.mme_addr=' + epc_ip + ' --enb.gtp_bind_addr=' + enb_ip + ' --enb.s1c_bind_addr=' + enb_ip
+    docker_config['services']['srsenb'][
+        'command'] = 'srsenb /etc/srsran/enb.conf.fauxrf --enb.mme_addr=' + epc_ip + ' --enb.gtp_bind_addr=' + enb_ip + ' --enb.s1c_bind_addr=' + enb_ip
 
-docker_config['services']['srsenb']['volumes'][
-    0] = './pcaps/' + test_number + ':/pcaps/'
+    docker_config['services']['srsenb']['volumes'][
+        0] = './pcaps/' + test_number + ':/pcaps/'
 
-docker_config['services']['srsenb']['networks']['corenet'][
-    'ipv4_address'] = enb_ip
+    docker_config['services']['srsenb']['networks']['corenet'][
+        'ipv4_address'] = enb_ip
 
-docker_config['networks']['corenet']['ipam']['config'][0]['subnet'] = subnet
+    docker_config['networks']['corenet']['ipam']['config'][0][
+        'subnet'] = subnet
 
-# container_name
-docker_config['services']['srsepc'][
-    'container_name'] = 'virtual-srsepc' + test_number
-docker_config['services']['srsenb'][
-    'container_name'] = 'virtual-srsenb' + test_number
-docker_config['services']['srsue'][
-    'container_name'] = 'virtual-srsue' + test_number
+    # container_name
+    docker_config['services']['srsepc'][
+        'container_name'] = 'virtual-srsepc' + test_number
+    docker_config['services']['srsenb'][
+        'container_name'] = 'virtual-srsenb' + test_number
+    docker_config['services']['srsue'][
+        'container_name'] = 'virtual-srsue' + test_number
 
-with open(sys.argv[3], 'w') as newconf:
-    yaml.dump(docker_config, newconf, default_flow_style=False)
+    with open(sys.argv[3], 'w') as newconf:
+        yaml.dump(docker_config, newconf, default_flow_style=False)
+
+    print("Generated Docker Compose for ")
+
+
+def generate_subnet_string(iterationNum):
+    """
+    when given current iteration, generate a subnet range.
+    each subnet is comprised of a /28 block, thus the last
+    4 bits of the ip address are available, along with
+    10.***.*** ranges, so 20 bits are available to us,
+    and about 1mil subnets can be created
+
+     Parameters:
+        iterationNum (int): Current Test Number
+
+    Returns:
+        subnet_string (str): /28 subnet for current iteration
+    """
+    first = str(iterationNum >> 12)  # first 8 bits [0-7]
+    second = str((iterationNum & 4080) >> 4)  # middle 8 bits [8 - 15]
+    third = str(
+        16 * (iterationNum & 15)
+    )  # final 4 bits, multiplied by 16 to obtein this iterations subnet
+    if iterationNum > 1048575:  # 20 bit max val
+        print(
+            "generate_subnet_string doesnt support more than 1mil iterations")
+        quit(1)
+
+    return "10." + first + "." + second + "." + third + "/28"
+
+
+def generate_ip(iterationNum, ipNum):
+    """
+        generate a valid ip within this current iteration's
+        subnet. /28 blocks allow for 15 ips maximum, thus ipNum
+        cannot be above 15
+
+
+     Parameters:
+        iterationNum (int): Current Test Number
+        ipNum (int): ip needed within block, valid values are 1 thru 15
+
+    Returns:
+        ip_address (str): ip within iterations subnet
+    """
+    first = str(iterationNum >> 12)  # first 8 bits [0-7]
+    second = str((iterationNum & 4080) >> 4)  # middle 8 bits [8 - 15]
+    # final 4 bits, multiplied by 16 to obtein this iterations subnet,
+    # then added to ipNum to obtain a unique ip
+    third = str((16 * (iterationNum & 15)) + ipNum)
+
+    return "10." + first + "." + second + "." + third
